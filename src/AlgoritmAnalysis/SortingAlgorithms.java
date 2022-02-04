10fc455a099c391f2bcd30bae2a6945063a10039
Kantonio Sean Brownlee <92135737+kantoniosean@users.noreply.github.com>
2022-02-03 23:23:24 -0600

    Update SortingAlgorithms.java

--- src/AlgoritmAnalysis/SortingAlgorithms.java
+++ src/AlgoritmAnalysis/SortingAlgorithms.java
@@ -155,7 +155,8 @@ public class SortingAlgorithms {
                 i++;
             else {
                 swap(arr, i, i - 1);
-                if(i > 1)i--;
+                if(i > 1)
+                    i--;
             }
         }
         return arr;
